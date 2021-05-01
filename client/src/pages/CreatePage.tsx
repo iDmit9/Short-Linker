506185aaa09d3a02214a042312d8bb2c3f3e654d
Dmitriy Ivanov <dmitriy.p86@ya.ru>
2021-05-01 18:45:22 +0600

    CreatePage-and-LinksPage-to-ts

--- client/src/pages/CreatePage.tsx
+++ client/src/pages/CreatePage.tsx
@@ -1,9 +1,21 @@
 import React, { useState, useContext } from 'react'
 import { useHistory } from 'react-router-dom'
+import { Layout, Row, Input, Col } from 'antd'
+
 import { useHttp } from '../hooks/http.hook'
 import { AuthContext } from '../context/AuthContext'
 
-import { Layout, Row, Input, Col } from 'antd'
+export type LinkType = {
+    link: {
+        clicks: number
+        code: string
+        date: string
+        from: string
+        owner: string
+        to: string
+        _id: string
+    }    
+}
 
 export const CreatePage = () => {
     const history = useHistory()
@@ -11,16 +23,16 @@ export const CreatePage = () => {
     const { request } = useHttp()
     const [link, setLink] = useState('')
 
-    const pressHandler = async event => {
+    const pressHandler = async (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === 'Enter') {
             try {
-                const data = await request('/api/link/generate', 'POST', { from: link }, {
+                const data: LinkType = await request('/api/link/generate', 'POST', { from: link }, {
                     Authorization: `Bearer ${auth.token}`
                 })
 
                 history.push(`/detail/${data.link._id}`)
             } catch (e) {
-
+                console.log('Link generation error')
             }
         }
     }
