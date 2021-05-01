17f29ffb4ec710c18539bd51a0f75ba2def6c462
Dmitriy Ivanov <dmitriy.p86@ya.ru>
2021-05-01 19:11:22 +0600

    DetailPage-typescripted

--- client/src/pages/DetailPage.tsx
+++ client/src/pages/DetailPage.tsx
@@ -1,19 +1,26 @@
 import React, { useState, useCallback, useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
+
 import { useHttp } from '../hooks/http.hook'
 import { AuthContext } from '../context/AuthContext'
 import { Loader } from '../components/Loader'
 import { LinkCard } from '../components/LinkCard'
 
+import { LinkType } from './CreatePage'
+
+type LinkParams = {
+    id: string;
+};
+
 export const DetailPage = () => {
     const { token } = useContext(AuthContext)
     const { request, loading } = useHttp()
-    const [link, setLink] = useState(null)
-    const linkId = useParams().id
+    const [ link, setLink ] = useState<LinkType | null>(null)
+    const linkId = useParams<LinkParams>().id
 
     const getLink = useCallback(async () => {
         try {
-            const fetched = await request(`/api/link/${linkId}`, 'GET', null, {
+            const fetched: LinkType = await request(`/api/link/${linkId}`, 'GET', null, {
                 Authorization: `Bearer ${token}`
             })
 
